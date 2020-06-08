8e5fdd07529b1d0926109c44d4c0bbdd887fb7b7
Mats Byrkjeland <matsbyr@gmail.com>
2020-06-08 16:51:05 +0200

    Setup TypeScript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,29 @@
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:node/recommended'],
-    plugins: ['prettier'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'prettier'],
+    extends: [
+        'eslint:recommended',
+        'plugin:node/recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
     parserOptions: {
         ecmaVersion: 2020,
+        sourceType: 'module',
     },
     env: {
         node: true,
     },
     rules: {
+        'node/no-unsupported-features/es-syntax': 'off',
+        'node/no-missing-import': 'off',
+        'node/shebang': [
+            'error',
+            {
+                convertPath: {
+                    'src/**/*.ts': ['^src/(.+?).ts$', 'dist/$1.js'],
+                },
+            },
+        ],
         'prefer-const': 'error',
         'prettier/prettier': 'error',
     },
--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,47 @@
 #!/usr/bin/env node
-const path = require('path')
-const commander = require('commander')
-const firebase = require('firebase-tools')
+import path from 'path'
+import commander from 'commander'
+import firebase from 'firebase-tools'
 
-const {
+import {
     readJsonFile,
     writeJsonFile,
     transformJsonConfigToFirebaseArgs,
     pickSameKeys,
     sortObject,
     parseConfigValues,
-} = require('./util')
+} from './util'
+
+import { ConfigFileLocal, ConfigFileRemote } from './interfaces'
 
 const program = new commander.Command()
 
-function logInfo(text) {
+function logInfo(text: string): void {
     if (!program.quiet) console.log(text)
 }
 
-function logError(text) {
+function logError(text: string): void {
     if (!program.quiet) console.error(text)
 }
 
-async function getConfigFiles() {
+async function getConfigFiles(): Promise<{ [project: string]: string }> {
     const { config } = program
     const filePath = path.resolve(config)
 
     try {
-        const configData = await readJsonFile(filePath)
+        const configData = await readJsonFile<{
+            configFiles: { [project: string]: string }
+        }>(filePath)
         return configData.configFiles
     } catch (error) {
         logError(
             'No config found. Please add "configFiles" to your .firebaserc',
         )
+        return {}
     }
 }
 
-async function getProjects(configFiles) {
+async function getProjects(configFiles: { [key: string]: any }) {
     const argProjects = program.project && program.project.trim().split(',')
     return Object.keys(configFiles).filter(
         (p) => !argProjects || argProjects.includes(p),
@@ -54,10 +59,15 @@ async function get() {
 
         logInfo(`Downloading config to ${configFile} from ${project}`)
 
-        let config = await firebase.functions.config.get(undefined, { project })
+        let config: ConfigFileRemote = await firebase.functions.config.get(
+            undefined,
+            { project },
+        )
 
         if (program.ignore) {
-            const existingConfig = await readJsonFile(configFile)
+            const existingConfig = await readJsonFile<ConfigFileLocal>(
+                configFile,
+            )
             config = pickSameKeys(config, existingConfig)
         }
 
@@ -82,7 +92,7 @@ async function set() {
 
         logInfo(`Uploading config to ${project} from ${configFile}`)
 
-        const config = await readJsonFile(configFile)
+        const config = await readJsonFile<ConfigFileLocal>(configFile)
         const parsed = transformJsonConfigToFirebaseArgs(config)
         await firebase.functions.config.set(parsed, { project })
 
